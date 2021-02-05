ce7f5207661554a4fd870516fdba383c296f7367
Achim <achim.schneider@equeo.de>
2021-02-05 19:23:09 +0100

    fixed references in react-util

--- packages/react-util/src/getContractForAddress.ts
+++ packages/react-util/src/getContractForAddress.ts
@@ -1,21 +1,19 @@
 // Copyright 2017-2021 @canvas-ui/app-contracts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { getContractAbi } from '@canvas-ui/react-util';
+import getContractAbi from "./getContractAbi";
 
-import { ApiPromise } from '@polkadot/api';
-import { ContractPromise as Contract } from '@polkadot/api-contract';
+import { ApiPromise } from "@polkadot/api";
+import { ContractPromise as Contract } from "@polkadot/api-contract";
 
-import { StringOrNull } from './types';
+import { StringOrNull } from "./types";
 
-export default function getContractForAddress (api: ApiPromise, address: StringOrNull): Contract | null {
+export default function getContractForAddress(api: ApiPromise, address: StringOrNull): Contract | null {
   if (!address) {
     return null;
   } else {
     const abi = getContractAbi(address);
 
-    return abi
-      ? new Contract(api, abi, address)
-      : null;
+    return abi ? new Contract(api, abi, address) : null;
   }
 }
--- packages/react-util/src/useEndpointOptions.tsx
+++ packages/react-util/src/useEndpointOptions.tsx
@@ -1,42 +1,38 @@
 // Copyright 2017-2021 @canvas-ui/app-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { createEndpoints } from '@canvas-ui/apps-config/settings';
-import { useApi } from '@canvas-ui/react-hooks';
-import { UseEndpoints } from '@canvas-ui/react-hooks/types';
-import { classes } from '@canvas-ui/react-util';
-import { TFunction } from 'i18next';
-import React, { useMemo } from 'react';
+import { createEndpoints } from "@canvas-ui/apps-config/settings";
+import { useApi } from "@canvas-ui/react-hooks";
+import { UseEndpoints } from "@canvas-ui/react-hooks/types";
+import classes from "./classes";
+import { TFunction } from "i18next";
+import React, { useMemo } from "react";
 
-export default function useEndpointOptions ({ isCustom, url }: UseEndpoints, t: TFunction, useShortText?: boolean): React.ReactNode[] {
+export default function useEndpointOptions(
+  { isCustom, url }: UseEndpoints,
+  t: TFunction,
+  useShortText?: boolean
+): React.ReactNode[] {
   const { isApiConnected } = useApi();
-  const className = classes('chain-option', !isApiConnected && 'isDisconnected');
+  const className = classes("chain-option", !isApiConnected && "isDisconnected");
 
   return useMemo(
-    () => ([
+    () => [
       ...createEndpoints(t).map(({ shortText, text, value }) => ({
         key: value,
-        text: (
-          <div className={className}>
-            {useShortText ? shortText : text}
-          </div>
-        ),
+        text: <div className={className}>{useShortText ? shortText : text}</div>,
         value
       })),
-      ...(
-        isCustom
-          ? [{
-            key: url,
-            text: (
-              <div className={className}>
-                {t<string>('Custom Node')}
-              </div>
-            ),
-            value: url
-          }]
-          : []
-      )
-    ]),
+      ...(isCustom
+        ? [
+            {
+              key: url,
+              text: <div className={className}>{t<string>("Custom Node")}</div>,
+              value: url
+            }
+          ]
+        : [])
+    ],
     [className, isCustom, t, url, useShortText]
   );
 }
