41333cf2034d77df70bf588d0b93d6f218639607
Achim <achim.schneider@equeo.de>
2021-02-06 11:48:29 +0100

    added missing deps to test-support

--- packages/test-support/src/api/createApi.ts
+++ packages/test-support/src/api/createApi.ts
@@ -1,12 +1,12 @@
 // Copyright 2017-2021 @canvas-ui/test-support authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { SUBSTRATE_PORT } from '@canvas-ui/test-support/substrate';
+import { SUBSTRATE_PORT } from '../substrate';
 
 import { ApiPromise } from '@polkadot/api/promise';
 import { WsProvider } from '@polkadot/rpc-provider';
 
-export async function createApi (): Promise<ApiPromise> {
+export async function createApi(): Promise<ApiPromise> {
   process.env.NODE_ENV = 'test';
 
   const provider = new WsProvider(`ws://127.0.0.1:${SUBSTRATE_PORT}`);
